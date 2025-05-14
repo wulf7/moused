e1264bad13c3abafdc6f20654260e3d04e4ec509
Vladimir Kondratyev <vladimir@kondratyev.su>
2025-05-14 21:33:04 +0300

    Move parts of util-strings.h from util.c to util.h to match libinput

--- util.h
+++ util.h
@@ -28,6 +28,13 @@
 #include <sys/types.h>
 #include <sys/mouse.h>
 
+#include <assert.h>
+#include <ctype.h>
+#include <math.h>
+#include <xlocale.h>
+
+#define	HAVE_LOCALE_H	1
+
 #define	versionsort	alphasort
 
 #define LIBINPUT_ATTRIBUTE_PRINTF(_format, _args) \
@@ -138,35 +145,266 @@ struct input_prop {
  */
 typedef void moused_log_handler(int priority, int errnum,
 				const char *format, va_list args);
-typedef int strv_foreach_callback_t(const char *str, size_t index, void *data);
 
-bool	streq(const char *str1, const char *str2);
-bool	strneq(const char *str1, const char *str2, int n);
-void *	zalloc(size_t size);
-char*	safe_strdup(const char *str);
+/* util-strings.h */
+
+static inline bool
+streq(const char *str1, const char *str2)
+{
+	/* one NULL, one not NULL is always false */
+	if (str1 && str2)
+		return strcmp(str1, str2) == 0;
+	return str1 == str2;
+}
+
+static inline bool
+strneq(const char *str1, const char *str2, int n)
+{
+	/* one NULL, one not NULL is always false */
+	if (str1 && str2)
+		return strncmp(str1, str2, n) == 0;
+	return str1 == str2;
+}
+
+static inline void *
+zalloc(size_t size)
+{
+	void *p;
+
+	/* We never need to alloc anything more than 1,5 MB so we can assume
+	 * if we ever get above that something's going wrong */
+	if (size > 1536 * 1024)
+		assert(!"bug: internal malloc size limit exceeded");
+
+	p = calloc(1, size);
+	if (!p)
+		abort();
+
+	return p;
+}
+
+/**
+ * strdup guaranteed to succeed. If the input string is NULL, the output
+ * string is NULL. If the input string is a string pointer, we strdup or
+ * abort on failure.
+ */
+static inline char*
+safe_strdup(const char *str)
+{
+	char *s;
+
+	if (!str)
+		return NULL;
+
+	s = strdup(str);
+	if (!s)
+		abort();
+	return s;
+}
+
+/**
+ * Simple wrapper for asprintf that ensures the passed in-pointer is set
+ * to NULL upon error.
+ * The standard asprintf() call does not guarantee the passed in pointer
+ * will be NULL'ed upon failure, whereas this wrapper does.
+ *
+ * @param strp pointer to set to newly allocated string.
+ * This pointer should be passed to free() to release when done.
+ * @param fmt the format string to use for printing.
+ * @return The number of bytes printed (excluding the null byte terminator)
+ * upon success or -1 upon failure. In the case of failure the pointer is set
+ * to NULL.
+ */
 __attribute__ ((format (printf, 2, 3)))
-int	xasprintf(char **strp, const char *fmt, ...);
-bool	safe_atoi_base(const char *str, int *val, int base);
+static inline int
+xasprintf(char **strp, const char *fmt, ...)
+{
+	int rc = 0;
+	va_list args;
+
+	va_start(args, fmt);
+	rc = vasprintf(strp, fmt, args);
+	va_end(args);
+	if ((rc == -1) && strp)
+		*strp = NULL;
+
+	return rc;
+}
+
+static inline bool
+safe_atoi_base(const char *str, int *val, int base)
+{
+	assert(str != NULL);
+
+	char *endptr;
+	long v;
+
+	assert(base == 10 || base == 16 || base == 8);
+
+	errno = 0;
+	v = strtol(str, &endptr, base);
+	if (errno > 0)
+		return false;
+	if (str == endptr)
+		return false;
+	if (*str != '\0' && *endptr != '\0')
+		return false;
+
+	if (v > INT_MAX || v < INT_MIN)
+		return false;
+
+	*val = v;
+	return true;
+}
+
 static inline bool
 safe_atoi(const char *str, int *val)
 {
-	return (safe_atoi_base(str, val, 10));
+	assert(str != NULL);
+	return safe_atoi_base(str, val, 10);
 }
-bool	safe_atou_base(const char *str, unsigned int *val, int base);
+
+static inline bool
+safe_atou_base(const char *str, unsigned int *val, int base)
+{
+	assert(str != NULL);
+
+	char *endptr;
+	unsigned long v;
+
+	assert(base == 10 || base == 16 || base == 8);
+
+	errno = 0;
+	v = strtoul(str, &endptr, base);
+	if (errno > 0)
+		return false;
+	if (str == endptr)
+		return false;
+	if (*str != '\0' && *endptr != '\0')
+		return false;
+
+	if ((long)v < 0)
+		return false;
+
+	*val = v;
+	return true;
+}
+
 static inline bool
 safe_atou(const char *str, unsigned int *val)
 {
-	return (safe_atou_base(str, val, 10));
+	assert(str != NULL);
+	return safe_atou_base(str, val, 10);
 }
-bool	safe_atod(const char *str, double *val);
-void	strv_free(char **strv);
-const char *next_word(const char **state, size_t *len, const char *separators);
-char **	strv_from_string(const char *in, const char *separators,
-	    size_t *num_elements);
-int	strv_for_each_n(const char **strv, size_t max,
-	    strv_foreach_callback_t func, void *data);
-bool	strendswith(const char *str, const char *suffix);
-bool	strstartswith(const char *str, const char *prefix);
+
+static inline bool
+safe_atod(const char *str, double *val)
+{
+	assert(str != NULL);
+
+	char *endptr;
+	double v;
+	size_t slen = strlen(str);
+
+	/* We don't have a use-case where we want to accept hex for a double
+	 * or any of the other values strtod can parse */
+	for (size_t i = 0; i < slen; i++) {
+		char c = str[i];
+
+		if (isdigit(c))
+		       continue;
+		switch(c) {
+		case '+':
+		case '-':
+		case '.':
+			break;
+		default:
+			return false;
+		}
+	}
+
+#ifdef HAVE_LOCALE_H
+	/* Create a "C" locale to force strtod to use '.' as separator */
+	locale_t c_locale = newlocale(LC_NUMERIC_MASK, "C", (locale_t)0);
+	if (c_locale == (locale_t)0)
+		return false;
+
+	errno = 0;
+	v = strtod_l(str, &endptr, c_locale);
+	freelocale(c_locale);
+#else
+	/* No locale support in provided libc, assume it already uses '.' */
+	errno = 0;
+	v = strtod(str, &endptr);
+#endif
+	if (errno > 0)
+		return false;
+	if (str == endptr)
+		return false;
+	if (*str != '\0' && *endptr != '\0')
+		return false;
+	if (v != 0.0 && !isnormal(v))
+		return false;
+
+	*val = v;
+	return true;
+}
+
+char **strv_from_string(const char *in, const char *separator, size_t *num_elements);
+
+typedef int (*strv_foreach_callback_t)(const char *str, size_t index, void *data);
+int strv_for_each_n(const char **strv, size_t max, strv_foreach_callback_t func, void *data);
+
+static inline void
+strv_free(char **strv) {
+	char **s = strv;
+
+	if (!strv)
+		return;
+
+	while (*s != NULL) {
+		free(*s);
+		*s = (char*)0x1; /* detect use-after-free */
+		s++;
+	}
+
+	free (strv);
+}
+
+/**
+ * Return true if str ends in suffix, false otherwise. If the suffix is the
+ * empty string, strendswith() always returns false.
+ */
+static inline bool
+strendswith(const char *str, const char *suffix)
+{
+	if (str == NULL)
+		return false;
+
+	size_t slen = strlen(str);
+	size_t suffixlen = strlen(suffix);
+	size_t offset;
+
+	if (slen == 0 || suffixlen == 0 || suffixlen > slen)
+		return false;
+
+	offset = slen - suffixlen;
+	return strneq(&str[offset], suffix, suffixlen);
+}
+
+static inline bool
+strstartswith(const char *str, const char *prefix)
+{
+	if (str == NULL)
+		return false;
+
+	size_t prefixlen = strlen(prefix);
+
+	return prefixlen > 0 ? strneq(str, prefix, strlen(prefix)) : false;
+}
+
+/* !util-strings.h */
+
 bool	parse_dimension_property(const char *prop, size_t *w, size_t *h);
 bool	parse_range_property(const char *prop, int *hi, int *lo);
 bool	parse_boolean_property(const char *prop, bool *b);
